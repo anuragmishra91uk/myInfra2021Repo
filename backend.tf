f5fe9419de86909f4f133272656ecfb1e6e170fb
Anurag Mishra <87974436+anuragmishra91uk@users.noreply.github.com>
2024-03-04 14:57:36 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name="cloud-shell-storage-centralindia-new"
     storage_account_name="csg100320016024556c"
-    container_name="mytfstatecontaineranurag"
+    container_name="mytfstatecontaineranura"
     key = "terraform.tfstate"
    
     
