a50727a22cd8c348f74055985d50ae328375df78
Anurag Mishra <87974436+anuragmishra91uk@users.noreply.github.com>
2024-03-04 13:45:26 +0530

    Update webapp.tf

--- webapp.tf
+++ webapp.tf
@@ -1,7 +1,7 @@
 # Create the resource group
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
-  location = "eastus"
+  location = var.location
 }
 
 # Create the Linux App Service Plan
